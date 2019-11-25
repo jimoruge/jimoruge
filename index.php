9e67d772287d7fa04232940ffd7f5609c7d19db6
jimoruge <jimoruge@yahoo.com>
2019-11-25 15:21:16 +0800

    hello world

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+
+<html>
+<head>
+	<title>hello world!</title>
+</head>
+
+<body>
+
+<?php
+
+print "Hello World!"
+
+?>
+
+</body>
+</html>
